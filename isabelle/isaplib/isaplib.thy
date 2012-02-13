e18f31245c4da8981e8e31fa66e86e13bebb0a72
Lucas Dixon <lucas.dixon@gmail.com>
2012-02-13 09:39:22 -0500

    added (bad) re-use of General/XML.ML to isabelle's isaplib.thy file. This overwrite's Isabelles version of this library. Needed to support CData.

--- isabelle/isaplib/isaplib.thy
+++ isabelle/isaplib/isaplib.thy
@@ -1,6 +1,8 @@
 theory isaplib
 imports Main
 uses
+(* TODO: remove xml being reloaded here. *)
+"../../General/xml.ML" 
 (* basic logging, testing and project tools. *)
 "../../basics/log.ML" 
 "../../project/project.ML" 
